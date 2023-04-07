0ead6970cf3dc6068d961f029f8ae7d2ab97fe2a
MunkushiR <ruth.munkushi@student.moringaschool.com>
2023-04-07 14:37:17 +0300

    done

--- index.js
+++ index.js
@@ -31,5 +31,25 @@ const postsUrl = "https://nameless-reef-64152.herokuapp.com/posts"
 
 
 
+// functions
+function getLatest() {
+    fetch(latestlUrl)
+     .then((response) => response.json())
+     .then(data => {
+        let output = '';
+        data.slice(0, 1).forEach(latest => {
+
+            output += `
+            <div class="md:flex justify-bewtween lg:space-x-32 md:space-x-8">
+                <div class="relative shadow-lg hover:shadow-xl hover:scale-105 hover:translate-2 hover:transform duration-500 text-center">
+                    <img class="rounded-lg w-full" src="${latest.urlToImage}" alt="">
+                    <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+                        <a class="md:text-4xl text-3xl text-white font-bold tracking-wide cursor-pointer hover:text-gray-100 href="${latest.url}">${latest.title}</a>
+                        <p class="md:text-2xl text-lg font-medium mb-2">${latest.description}</p>
+                        <h3 class="text-lg py-2 font-semibold text-green-200">${latest.author}, <span class="font-medium">${latest.name}</span></h3>
+                    </div>
+                </div>
+                
+
     
 }
